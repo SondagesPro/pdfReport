5193cb57c2e7e9e4f2e7823ce810d34a1201b85f
Denis Chenu <denis@sondages.pro>
2017-02-25 18:41:26 +0100

    [wip] Remove uneeded function, rename others, add question attribute

--- pdfReport.php
+++ pdfReport.php
@@ -31,309 +31,39 @@ class pdfReport extends \ls\pluginmanager\PluginBase {
 
     public function init()
     {
-        $this->subscribe('beforeSurveySettings');
-        $this->subscribe('newSurveySettings');
-        $this->subscribe('afterSurveyComplete', 'mailPdfReport');
+
     }
     protected $settings = array(
-        'filenameend'=>array(
-            'type'=>'select',
-            'label'=>'Use token (if exist) for file name.',
-            'options'=>array(
-                'token'=>'Yes',
-                'responseid'=>'No',
-            ),
-            'default'=>'token',
-        ),
-        'name_confirm'=>array(
-            'type'=>'string',
-            'label'=>'Base file name for confirmation pdf file',
-            'default'=>'confirm',
-        ),
-        'name_admin_notification'=>array(
-            'type'=>'string',
-            'label'=>'Base file name for admin_notification pdf file',
-            'default'=>'admin_notification',
-        ),
-        'name_admin_responses'=>array(
-            'type'=>'string',
-            'label'=>'Base file name for admin_responses pdf file',
-            'default'=>'admin_responses',
-        ),
         'basesavedirectory'=>array(
             'type'=>'string',
             'label'=>'Directory on the server to move the file after send (set to empty to remove the file)',
+            'help'=>'You can use {SID} for survey id. Plugin didn`t create directory.',
             'default'=>'',
         ),
 
     );
+
     /**
-     * This event is fired by the administration panel to gather extra settings
-     * available for a survey.
-     * The plugin should return setting meta data.
-     * @param PluginEvent $event
+     * Get a pdf file from a string
+     * @param integer $iQid
+     * @return string : URI for pdf file
      */
-    public function beforeSurveySettings()
+    private function _getPdfFile($iQid)
     {
-        $event = $this->event;
-        $aLogo=$this->getLogoPaths();
-        if(!empty($aLogo['url'])){
-            $sLogoComplement="&nbsp;<img src='{$aLogo['url']}' alt='{$aLogo['url']}' style='max-width:8em;max-height:2em;' />";
-        }elseif(!empty($aLogo['error'])){
-            $sLogoComplement="&nbsp;<span class='label label-warning'>{$aLogo['error']}</span>";
-        }else{
-            $sLogoComplement="";
+        $oQuestion=Question::model()->findByPk(array('qid'=>$iQid,'language'=>$this->sLanguage);
+        if(!$oQuestion){
+            Yii::log("Question number {$iQid} invalid",'error','application.plugins.sendPdfReport');
+            return null;
         }
-        $event->set("surveysettings.{$this->id}", array(
-            'name' => get_class($this),
-            'settings' => array(
-                'pdf_info' => array(
-                    'type'=> 'info',
-                    'content'=>"<p class='alert alert-info'><strong>Attention</strong> : Don't use default mail sending by LimeSurvey and set Send confirmation emails? to NO if you use confirmation email.</p>",
-                ),
-                'pdf_global' => array(
-                    'type'=> 'info',
-                    'content'=>"<p class='label label-info'>".gt("Global settings")."</p>",
-                ),
-                'pdf_header' => array(
-                    'type'=> 'string',
-                    'label'=>gt("PDF title"),
-                    'current'=>$this->get('pdf_header','Survey',$event->get('survey'),"{SITENAME}"),
-                ),
-                'pdf_subheader' => array(
-                    'type'=> 'string',
-                    'label'=>gt("PDF sub title"),
-                    'current'=>$this->get('pdf_subheader','Survey',$event->get('survey'),"{SURVEYNAME}"),
-                ),
-                'pdf_logo' => array(
-                    'type'=> 'string',
-                    'label'=>gt("Logo Name").$sLogoComplement,
-                    'current'=>$this->get('pdf_logo','Survey',$event->get('survey')),
-                ),
-                'pdf_confirm' => array(
-                    'type'=> 'info',
-                    'content'=>"<p class='label label-info'>".gt("Confirmation email")."</p>",
-                ),
-                'pdf_confirm_to' => array(
-                    'type'=> 'string',
-                    'label'=>gt("Send confirmation emails?"),
-                    'current'=>$this->get('pdf_confirm_to','Survey',$event->get('survey')),
-                ),
-                'pdf_confirm_attachment' => array(
-                    'type'=> 'string',
-                    'label'=>gt("Confirmation attachments:")." (".gt("Question code").")",
-                    'current'=>$this->get('pdf_confirm_attachment','Survey',$event->get('survey')),
-                ),
-                'pdf_admin_notification' => array(
-                    'type'=> 'info',
-                    'content'=>"<p class='label label-info'>".gt("Basic admin notification")."</p>",
-                ),
-                'pdf_admin_notification_to' => array(
-                    'type'=> 'string',
-                    'label'=>gt("Basic email notification is sent to:"),
-                    'current'=>$this->get('pdf_admin_notification_to','Survey',$event->get('survey')),
-                ),
-                'pdf_admin_notification_attachment' => array(
-                    'type'=> 'string',
-                    'label'=>gt("Basic notification attachments:")." (".gt("Question code").")",
-                    'current'=>$this->get('pdf_admin_notification_attachment','Survey',$event->get('survey')),
-                ),
-                'pdf_admin_responses' => array(
-                    'type'=> 'info',
-                    'content'=>"<p class='label label-info'>".gt("Detailed admin notification")."</p>",
-                ),
-                'pdf_admin_responses_to' => array(
-                    'type'=> 'string',
-                    'label'=>gt("Send detailed admin notification email to: "),
-                    'current'=>$this->get('pdf_admin_responses_to','Survey',$event->get('survey')),
-                ),
-                'pdf_admin_responses_attachment' => array(
-                    'type'=> 'string',
-                    'label'=>gt("Detailed notification attachments:")." (".gt("Question code").")",
-                    'current'=>$this->get('pdf_admin_responses_attachment','Survey',$event->get('survey')),
-                ),
-
-            )
-        ));
-    }
-    public function newSurveySettings()
-    {
-        //~ parent::newSurveySettings();
-        $event = $this->event;
-        foreach ($event->get('settings') as $name => $value)
-        {
-            $this->set($name, $value, 'Survey', $event->get('survey'));
-        }
-        // Maybe fix Survey ?
-    }
-    /*
-     * Below are the actual methods that handle events
-     */
-    public function mailPdfReport()
-    {
-        $oEvent      = $this->getEvent();
-        $this->iSurveyId=$iSurveyId   = $oEvent->get('surveyId');
-        $this->iResponseId=$iResponseId = $oEvent->get('responseId');
-        $aMailType=array("confirm","admin_notification","admin_responses");
-        $oSurvey=Survey::model()->findByPk($iSurveyId);
-
-        $aSettings=array();
-        foreach($aMailType as $sMailType)
-        {
-            if($this->get("pdf_{$sMailType}_to", 'Survey', $iSurveyId)){
-                $aSettings[$sMailType]=array(
-                    "to"=>$this->get("pdf_{$sMailType}_to", 'Survey', $iSurveyId),
-                    "attachement"=>$this->get("pdf_{$sMailType}_attachment", 'Survey', $iSurveyId),
-                );
-            }
-            else{
-                $aSettings[$sMailType]=null;
-            }
-        }
-
-        if(count($aSettings)==0){
-            return;
-        }
-        if($iResponseId===null){
-            return;
-        }
-        $oSessionSurvey=Yii::app()->session["survey_{$iSurveyId}"];
-        if(!isset($oSessionSurvey['s_lang'])){
-            return;
-        }
-        $this->sLanguage=$sLanguage=$oSessionSurvey['s_lang'];
-        foreach($aSettings as $sType=>$aMailing)
-        {
-            //$oQuestion=Question::model()->find("sid=:sid and language=:language and title=:title",array(":sid"=>$iSurveyId,":language"=>$sLanguage,":title"=>$aMailing['attachement']));
-            $aRecipient=explode(";", ReplaceFields($aMailing['to'],array(), true));
-            $aValidRecipient=array();
-            foreach($aRecipient as $sRecipient)
-            {
-                $sRecipient=trim($sRecipient);
-                if(validateEmailAddress($sRecipient))
-                {
-                    $aValidRecipient[]=$sRecipient;
-                }
-            }
-            $aMessage=$this->getEmailContent($sType);
-            $aAttachments = array();
-            if($sFile=$this->getPdfFile($sType))
-            {
-                $aAttachments[]=$sFile;
-
-            }
-            foreach ($aValidRecipient as $sRecipient)
-            {
-                if (!SendEmailMessage($aMessage['message'], $aMessage['subject'],$sRecipient,"{$oSurvey->admin} <{$oSurvey->adminemail}>" , Yii::app()->getConfig("sitename"), true, getBounceEmail($iSurveyId), $aAttachments))
-                {
-                    Yii::log("Email with ".$sFile." can not be sent due to a mail error",'error','application.plugins.sendPdfReport');
-                }
-                else
-                {
-                    Yii::log("Email with ".$sFile." sent",'info','application.plugins.sendPdfReport');
-                }
-            }
-            if($sFile)
-            {
-                if($sBaseDir=$this->get("basesavedirectory"))
-                {
-                    if(is_dir($sBaseDir) && is_writable($sBaseDir))
-                    {
-                        $sBaseDir=rtrim($sBaseDir,DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
-                        if(rename($sFile,$sBaseDir.basename($sFile))){
-                            Yii::log("File is now at ".$sBaseDir.basename($sFile),'info','application.plugins.sendPdfReport');
-                        }else{
-                            Yii::log("An error happen when try to move to $sBaseDir",'error','application.plugins.sendPdfReport');
-                        }
-                    }
-                    else
-                    {
-                        Yii::log("Invalid directoty $sBaseDir",'error','application.plugins.sendPdfReport');
-                        @unlink($sFile);
-                    }
-                }
-                else{
-                    @unlink($sFile);
-                }
-            }
-        }
-    }
-    private function getEmailContent($sType)
-    {
-        $thissurvey=$aSurvey=getSurveyInfo($this->iSurveyId,$this->sLanguage);
-        $aReplacementVars=$this->getReplacementVars($sType=='confirm');
-
-        //~ $sSubject=LimeExpressionManager::ProcessString($aSurvey["email_{$sType}_subj"], NULL, $aReplacementVars, false, 3, 0, false, false, true);
-        //~ $sMessage=LimeExpressionManager::ProcessString($aSurvey["email_{$sType}"], NULL, $aReplacementVars, false, 3, 0, false, false, true);
         $aReData=array(
             'saved_id'=>$this->iResponseId,
             'thissurvey'=>getSurveyInfo($this->iSurveyId,$this->sLanguage),
         );
-        $sSubject=templatereplace($aSurvey["email_{$sType}_subj"],$aReplacementVars,$aReData,'',false,null,array(),true);
-        $sMessage=templatereplace($aSurvey["email_{$sType}"],$aReplacementVars,$aReData,'',false,null,array(),true);
+        $sHeader=>$oQuestion->question;
+        $sSubHeader=>$oQuestion->help;
 
-        return array(
-            'subject'=>$sSubject,
-            'message'=>$sMessage,
-        );
-    }
-    private function getReplacementVars($bWithToken=false)
-    {
-        static $aReplacementVars;
-        if(!empty($aReplacementVars))
-            return $aReplacementVars;
-        $thissurvey=$aSurvey=getSurveyInfo($this->iSurveyId,$this->sLanguage);
-        $aReplacementVars=array();
-        $aReplacementVars['RELOADURL']='';
-        $aReplacementVars['ADMINNAME'] = $aSurvey['adminname'];
-        $aReplacementVars['ADMINEMAIL'] = $aSurvey['adminemail'];
-        $aReplacementVars['VIEWRESPONSEURL']=Yii::app()->createAbsoluteUrl("/admin/responses/sa/view/surveyid/{$this->iSurveyId}/id/{$this->iResponseId}");
-        $aReplacementVars['EDITRESPONSEURL']=Yii::app()->createAbsoluteUrl("/admin/dataentry/sa/editdata/subaction/edit/surveyid/{$this->iSurveyId}/id/{$this->iResponseId}");
-        $aReplacementVars['STATISTICSURL']=Yii::app()->createAbsoluteUrl("/admin/statistics/sa/index/surveyid/{$this->iSurveyId}");
-        // Always HTML
-        if (true)
-        {
-            $aReplacementVars['VIEWRESPONSEURL']="<a href='{$aReplacementVars['VIEWRESPONSEURL']}'>{$aReplacementVars['VIEWRESPONSEURL']}</a>";
-            $aReplacementVars['EDITRESPONSEURL']="<a href='{$aReplacementVars['EDITRESPONSEURL']}'>{$aReplacementVars['EDITRESPONSEURL']}</a>";
-            $aReplacementVars['STATISTICSURL']="<a href='{$aReplacementVars['STATISTICSURL']}'>{$aReplacementVars['STATISTICSURL']}</a>";
-        }
-        $aReplacementVars['ANSWERTABLE']='';
-        $oSessionSurvey=Yii::app()->session["survey_{$this->iSurveyId}"];
-        if($bWithToken && !empty($oSessionSurvey['token']) && tableExists('{{tokens_' . $this->iSurveyId . '}}'))
-        {
-            $oToken=Token::model($this->iSurveyId)->find("token=:token",array('token' => $oSessionSurvey['token']));
-            if($oToken)
-            {
-                foreach($oToken->attributes as $attribute=>$value)
-                {
-                    $aReplacementVars[strtoupper($attribute)]=$value;
-                }
-            }
-        }
-        return $aReplacementVars;
-    }
-    private function getPdfFile($sType)
-    {
-        Yii::log("getPdfFile start for {$sType} in {$this->iSurveyId}",'trace','application.plugins.sendPdfReport');
-        $sQuestionCode=$this->get("pdf_{$sType}_attachment", 'Survey', $this->iSurveyId);
-
-        if(empty($sQuestionCode))
-            return;
-        $oQuestion=Question::model()->find("sid=:sid AND title=:title AND language=:language",array(':sid'=>$this->iSurveyId,':title'=>$sQuestionCode,':language'=>$this->sLanguage));
-        if(empty($oQuestion))
-            return;
-        if(!LimeExpressionManager::ProcessString("{".$oQuestion->title.".relevanceStatus}"))
-            return ;
-        $aReData=array(
-            'saved_id'=>$this->iResponseId,
-            'thissurvey'=>getSurveyInfo($this->iSurveyId,$this->sLanguage),
-        );
-        $sText=templatereplace($oQuestion->question,array(),$aReData,'',false,null,array(),true);
-        $sHeader = $this->get("pdf_header", 'Survey', $this->iSurveyId,"{SITENAME}");
         $sHeader=templatereplace($sHeader,array(),$aReData,'',false,null,array(),true);
-        $sSubHeader = $this->get("pdf_subheader", 'Survey', $this->iSurveyId,"{SURVEYNAME}");
         $sSubHeader=templatereplace($sSubHeader,array(),$aReData,'',false,null,array(),true);
-
 
         //~ return;
         $sCssContent=file_get_contents(dirname(__FILE__).'/base.css');
@@ -347,7 +77,7 @@ class pdfReport extends \ls\pluginmanager\PluginBase {
 
         Yii::import('application.libraries.admin.pdf', true);
         Yii::import('application.helpers.pdfHelper');
-        Yii::import("sendPdfReport.pdfReport");
+        Yii::import("sendPdfReport.helpers.pdfReportHelper");
 
         $aPdfLanguageSettings=pdfHelper::getPdfLanguageSettings($this->sLanguage);
 
@@ -408,7 +138,7 @@ class pdfReport extends \ls\pluginmanager\PluginBase {
         $sText="<style>\n{$sCssContent}\n</style>\n$sText\n";
         //~ $this->event->getContent($this)
               //~ ->addContent(htmlentities($sText));
-        $aLogo=$this->getLogoPaths();
+        $aLogo=$this->_getLogoPaths();
         if(!empty($aLogo['path']))
            $oPDF->sLogoFile=$aLogo['path'];
 
@@ -429,19 +159,17 @@ class pdfReport extends \ls\pluginmanager\PluginBase {
             $sFilePdfName.="{$this->iResponseId}.pdf";
         }
         $oPDF->Output($sFilePdfName, 'F');
-        Yii::log("getPdfFile done for {$sType} in {$this->iSurveyId}",'trace','application.plugins.sendPdfReport');
+        Yii::log("getPdfFile done for {$iQid} in {$this->iSurveyId}",'trace','application.plugins.sendPdfReport');
         return $sFilePdfName;
     }
-    private function getLogoPaths()
+    /**
+     * Get the logo file name
+     * @return string : URI for pdf file
+     */
+    private function _getLogoPaths($iSurveyId)
     {
+        $sLogoName='logo.png'; // @todo search for array (png|jpg|gif)
 
-        $iSurveyId=$this->event->get('survey');
-        if(empty($iSurveyId))
-            $iSurveyId=$this->event->get('surveyId');
-        $sLogoName=$this->get('pdf_logo','Survey',$iSurveyId);
-
-        if(empty($sLogoName))
-            return "empty";
         if(basename($sLogoName)!=$sLogoName)
         {
             return array('error'=>"Use cleaner file name (no / or \)");
@@ -482,55 +210,5 @@ class pdfReport extends \ls\pluginmanager\PluginBase {
             );
         }
         return array('error'=>"File not found in your survey.");
-    }
-
-    public function saveSettings($settings)
-    {
-        if(isset($settings['basesavedirectory']) && !empty($settings['basesavedirectory']))
-        {
-            if (!is_dir($settings['basesavedirectory']))
-            {
-                Yii::app()->setFlashMessage("Directory not found, base directory is set to none",'error');
-                $settings['basesavedirectory']="";
-            }
-            elseif (!is_writable($settings['basesavedirectory']))
-            {
-                Yii::app()->setFlashMessage("Directory found but not writable, base directory is set to none",'error');
-                $settings['basesavedirectory']="";
-            }
-        }
-        $updated =false;
-        if(isset($settings['name_confirm']))
-        {
-            $settings['name_confirm']=preg_replace('/[^a-z0-9-]/i', '', $settings['name_confirm']);
-            if(empty($settings['name_confirm']))
-            {
-                $settings['name_confirm']=$this->settings['name_confirm']['default'];
-                $updated=true;
-            }
-        }
-        if(isset($settings['name_admin_notification']))
-        {
-            $settings['name_admin_notification']=preg_replace('/[^a-z0-9-]/i', '', $settings['name_admin_notification']);
-            if(empty($settings['name_admin_notification']) || $settings['name_admin_notification']==$settings['name_confirm'])
-            {
-                $settings['name_admin_notification']=$this->settings['name_admin_notification']['default'];
-                $updated=true;
-            }
-        }
-        if(isset($settings['name_admin_responses']))
-        {
-            $settings['name_admin_responses']=preg_replace('/[^a-z0-9-]/i', '', $settings['name_admin_responses']);
-            if(empty($settings['name_admin_responses']) || $settings['name_admin_responses']==$settings['name_confirm'] || $settings['name_admin_responses']==$settings['name_admin_notification'] )
-            {
-                $settings['name_admin_responses']=$this->settings['name_admin_responses']['default'];
-                $updated=true;
-            }
-        }
-        if($updated){
-            Yii::app()->setFlashMessage("One or more of PDF file name was updated. Review the filenames.",'error');
-        }
-
-        parent::saveSettings($settings);
     }
 }

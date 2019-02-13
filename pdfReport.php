ace963826e0f0b3edc1c87933368118635c85107
Denis Chenu <denis@sondages.pro>
2019-02-13 11:45:02 +0100

    [fix] Broke if Transliterator don't exist

--- pdfReport.php
+++ pdfReport.php
@@ -8,7 +8,7 @@
  * @copyright 2017 Réseau en scène Languedoc-Roussillon <https://www.reseauenscene.fr/>
  * @copyright 2015 Ingeus <http://www.ingeus.fr/>
  * @license AGPL v3
- * @version 1.6.0
+ * @version 1.6.1
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -767,6 +767,7 @@ class pdfReport extends PluginBase {
             );
             $sanitize = empty($oQuestionAttributeFilter) ? 'base' : $oQuestionAttributeFilter->value;
             $alphanumeric = false;
+            $lowercase = false;
             $beautify = false;
             switch($sanitize) {
                 case "none":
@@ -776,8 +777,7 @@ class pdfReport extends PluginBase {
                 case "alphanumeric":
                     $reportSavedFileName = preg_replace('/\s+/', App()->getConfig("pdfReportSpaceFilename","-"), $reportSavedFileName);
                     /* replace accent see https://stackoverflow.com/a/16022459/2239406 */
-                    $myTrans = Transliterator::create('Any-Latin; Latin-ASCII; [\u0080-\u7fff] remove');
-                    $reportSavedFileName = $myTrans->transliterate($reportSavedFileName);
+                    $reportSavedFileName = $this->_transliterate($reportSavedFileName);
                     /* beautify_filename from santitize set lowercase : we don't want this … */
                     $reportSavedFileName = preg_replace(array('/ +/','/_+/','/-+/'), '-', $reportSavedFileName);
                     $reportSavedFileName = preg_replace(array('/-*\.-*/','/\.{2,}/'), '.', $reportSavedFileName);
@@ -938,5 +938,364 @@ class pdfReport extends PluginBase {
             return \LimeExpressionManager::ProcessString($string, null, $replacementFields, 3, 0, false, false, true);
         }
         return \LimeExpressionManager::ProcessStepString($string, true, 3, $replacementFields);
+    }
+
+    /**
+     * Transliterate
+     * @param string
+     * @return string
+     */
+    private function _transliterate($string)
+    {
+        if(class_exists("Transliterator")) {
+            $myTrans = Transliterator::create('Any-Latin; Latin-ASCII; [\u0080-\u7fff] remove');
+            return $myTrans->transliterate($string);
+        }
+        /* from https://github.com/WordPress/WordPress/blob/dc8e9c6de0df706fee45bc82ac26cbc4fbcb8a7f/wp-includes/formatting.php#L1596 */
+        $chars = array(
+            // Decompositions for Latin-1 Supplement
+            'ª' => 'a',
+            'º' => 'o',
+            'À' => 'A',
+            'Á' => 'A',
+            'Â' => 'A',
+            'Ã' => 'A',
+            'Ä' => 'A',
+            'Å' => 'A',
+            'Æ' => 'AE',
+            'Ç' => 'C',
+            'È' => 'E',
+            'É' => 'E',
+            'Ê' => 'E',
+            'Ë' => 'E',
+            'Ì' => 'I',
+            'Í' => 'I',
+            'Î' => 'I',
+            'Ï' => 'I',
+            'Ð' => 'D',
+            'Ñ' => 'N',
+            'Ò' => 'O',
+            'Ó' => 'O',
+            'Ô' => 'O',
+            'Õ' => 'O',
+            'Ö' => 'O',
+            'Ù' => 'U',
+            'Ú' => 'U',
+            'Û' => 'U',
+            'Ü' => 'U',
+            'Ý' => 'Y',
+            'Þ' => 'TH',
+            'ß' => 's',
+            'à' => 'a',
+            'á' => 'a',
+            'â' => 'a',
+            'ã' => 'a',
+            'ä' => 'a',
+            'å' => 'a',
+            'æ' => 'ae',
+            'ç' => 'c',
+            'è' => 'e',
+            'é' => 'e',
+            'ê' => 'e',
+            'ë' => 'e',
+            'ì' => 'i',
+            'í' => 'i',
+            'î' => 'i',
+            'ï' => 'i',
+            'ð' => 'd',
+            'ñ' => 'n',
+            'ò' => 'o',
+            'ó' => 'o',
+            'ô' => 'o',
+            'õ' => 'o',
+            'ö' => 'o',
+            'ø' => 'o',
+            'ù' => 'u',
+            'ú' => 'u',
+            'û' => 'u',
+            'ü' => 'u',
+            'ý' => 'y',
+            'þ' => 'th',
+            'ÿ' => 'y',
+            'Ø' => 'O',
+            // Decompositions for Latin Extended-A
+            'Ā' => 'A',
+            'ā' => 'a',
+            'Ă' => 'A',
+            'ă' => 'a',
+            'Ą' => 'A',
+            'ą' => 'a',
+            'Ć' => 'C',
+            'ć' => 'c',
+            'Ĉ' => 'C',
+            'ĉ' => 'c',
+            'Ċ' => 'C',
+            'ċ' => 'c',
+            'Č' => 'C',
+            'č' => 'c',
+            'Ď' => 'D',
+            'ď' => 'd',
+            'Đ' => 'D',
+            'đ' => 'd',
+            'Ē' => 'E',
+            'ē' => 'e',
+            'Ĕ' => 'E',
+            'ĕ' => 'e',
+            'Ė' => 'E',
+            'ė' => 'e',
+            'Ę' => 'E',
+            'ę' => 'e',
+            'Ě' => 'E',
+            'ě' => 'e',
+            'Ĝ' => 'G',
+            'ĝ' => 'g',
+            'Ğ' => 'G',
+            'ğ' => 'g',
+            'Ġ' => 'G',
+            'ġ' => 'g',
+            'Ģ' => 'G',
+            'ģ' => 'g',
+            'Ĥ' => 'H',
+            'ĥ' => 'h',
+            'Ħ' => 'H',
+            'ħ' => 'h',
+            'Ĩ' => 'I',
+            'ĩ' => 'i',
+            'Ī' => 'I',
+            'ī' => 'i',
+            'Ĭ' => 'I',
+            'ĭ' => 'i',
+            'Į' => 'I',
+            'į' => 'i',
+            'İ' => 'I',
+            'ı' => 'i',
+            'Ĳ' => 'IJ',
+            'ĳ' => 'ij',
+            'Ĵ' => 'J',
+            'ĵ' => 'j',
+            'Ķ' => 'K',
+            'ķ' => 'k',
+            'ĸ' => 'k',
+            'Ĺ' => 'L',
+            'ĺ' => 'l',
+            'Ļ' => 'L',
+            'ļ' => 'l',
+            'Ľ' => 'L',
+            'ľ' => 'l',
+            'Ŀ' => 'L',
+            'ŀ' => 'l',
+            'Ł' => 'L',
+            'ł' => 'l',
+            'Ń' => 'N',
+            'ń' => 'n',
+            'Ņ' => 'N',
+            'ņ' => 'n',
+            'Ň' => 'N',
+            'ň' => 'n',
+            'ŉ' => 'n',
+            'Ŋ' => 'N',
+            'ŋ' => 'n',
+            'Ō' => 'O',
+            'ō' => 'o',
+            'Ŏ' => 'O',
+            'ŏ' => 'o',
+            'Ő' => 'O',
+            'ő' => 'o',
+            'Œ' => 'OE',
+            'œ' => 'oe',
+            'Ŕ' => 'R',
+            'ŕ' => 'r',
+            'Ŗ' => 'R',
+            'ŗ' => 'r',
+            'Ř' => 'R',
+            'ř' => 'r',
+            'Ś' => 'S',
+            'ś' => 's',
+            'Ŝ' => 'S',
+            'ŝ' => 's',
+            'Ş' => 'S',
+            'ş' => 's',
+            'Š' => 'S',
+            'š' => 's',
+            'Ţ' => 'T',
+            'ţ' => 't',
+            'Ť' => 'T',
+            'ť' => 't',
+            'Ŧ' => 'T',
+            'ŧ' => 't',
+            'Ũ' => 'U',
+            'ũ' => 'u',
+            'Ū' => 'U',
+            'ū' => 'u',
+            'Ŭ' => 'U',
+            'ŭ' => 'u',
+            'Ů' => 'U',
+            'ů' => 'u',
+            'Ű' => 'U',
+            'ű' => 'u',
+            'Ų' => 'U',
+            'ų' => 'u',
+            'Ŵ' => 'W',
+            'ŵ' => 'w',
+            'Ŷ' => 'Y',
+            'ŷ' => 'y',
+            'Ÿ' => 'Y',
+            'Ź' => 'Z',
+            'ź' => 'z',
+            'Ż' => 'Z',
+            'ż' => 'z',
+            'Ž' => 'Z',
+            'ž' => 'z',
+            'ſ' => 's',
+            // Decompositions for Latin Extended-B
+            'Ș' => 'S',
+            'ș' => 's',
+            'Ț' => 'T',
+            'ț' => 't',
+            // Euro Sign
+            '€' => 'E',
+            // GBP (Pound) Sign
+            '£' => '',
+            // Vowels with diacritic (Vietnamese)
+            // unmarked
+            'Ơ' => 'O',
+            'ơ' => 'o',
+            'Ư' => 'U',
+            'ư' => 'u',
+            // grave accent
+            'Ầ' => 'A',
+            'ầ' => 'a',
+            'Ằ' => 'A',
+            'ằ' => 'a',
+            'Ề' => 'E',
+            'ề' => 'e',
+            'Ồ' => 'O',
+            'ồ' => 'o',
+            'Ờ' => 'O',
+            'ờ' => 'o',
+            'Ừ' => 'U',
+            'ừ' => 'u',
+            'Ỳ' => 'Y',
+            'ỳ' => 'y',
+            // hook
+            'Ả' => 'A',
+            'ả' => 'a',
+            'Ẩ' => 'A',
+            'ẩ' => 'a',
+            'Ẳ' => 'A',
+            'ẳ' => 'a',
+            'Ẻ' => 'E',
+            'ẻ' => 'e',
+            'Ể' => 'E',
+            'ể' => 'e',
+            'Ỉ' => 'I',
+            'ỉ' => 'i',
+            'Ỏ' => 'O',
+            'ỏ' => 'o',
+            'Ổ' => 'O',
+            'ổ' => 'o',
+            'Ở' => 'O',
+            'ở' => 'o',
+            'Ủ' => 'U',
+            'ủ' => 'u',
+            'Ử' => 'U',
+            'ử' => 'u',
+            'Ỷ' => 'Y',
+            'ỷ' => 'y',
+            // tilde
+            'Ẫ' => 'A',
+            'ẫ' => 'a',
+            'Ẵ' => 'A',
+            'ẵ' => 'a',
+            'Ẽ' => 'E',
+            'ẽ' => 'e',
+            'Ễ' => 'E',
+            'ễ' => 'e',
+            'Ỗ' => 'O',
+            'ỗ' => 'o',
+            'Ỡ' => 'O',
+            'ỡ' => 'o',
+            'Ữ' => 'U',
+            'ữ' => 'u',
+            'Ỹ' => 'Y',
+            'ỹ' => 'y',
+            // acute accent
+            'Ấ' => 'A',
+            'ấ' => 'a',
+            'Ắ' => 'A',
+            'ắ' => 'a',
+            'Ế' => 'E',
+            'ế' => 'e',
+            'Ố' => 'O',
+            'ố' => 'o',
+            'Ớ' => 'O',
+            'ớ' => 'o',
+            'Ứ' => 'U',
+            'ứ' => 'u',
+            // dot below
+            'Ạ' => 'A',
+            'ạ' => 'a',
+            'Ậ' => 'A',
+            'ậ' => 'a',
+            'Ặ' => 'A',
+            'ặ' => 'a',
+            'Ẹ' => 'E',
+            'ẹ' => 'e',
+            'Ệ' => 'E',
+            'ệ' => 'e',
+            'Ị' => 'I',
+            'ị' => 'i',
+            'Ọ' => 'O',
+            'ọ' => 'o',
+            'Ộ' => 'O',
+            'ộ' => 'o',
+            'Ợ' => 'O',
+            'ợ' => 'o',
+            'Ụ' => 'U',
+            'ụ' => 'u',
+            'Ự' => 'U',
+            'ự' => 'u',
+            'Ỵ' => 'Y',
+            'ỵ' => 'y',
+            // Vowels with diacritic (Chinese, Hanyu Pinyin)
+            'ɑ' => 'a',
+            // macron
+            'Ǖ' => 'U',
+            'ǖ' => 'u',
+            // acute accent
+            'Ǘ' => 'U',
+            'ǘ' => 'u',
+            // caron
+            'Ǎ' => 'A',
+            'ǎ' => 'a',
+            'Ǐ' => 'I',
+            'ǐ' => 'i',
+            'Ǒ' => 'O',
+            'ǒ' => 'o',
+            'Ǔ' => 'U',
+            'ǔ' => 'u',
+            'Ǚ' => 'U',
+            'ǚ' => 'u',
+            // grave accent
+            'Ǜ' => 'U',
+            'ǜ' => 'u',
+        );
+        $chars['Ä'] = 'Ae';
+        $chars['ä'] = 'ae';
+        $chars['Ö'] = 'Oe';
+        $chars['ö'] = 'oe';
+        $chars['Ü'] = 'Ue';
+        $chars['ü'] = 'ue';
+        $chars['ß'] = 'ss';
+        $chars['Æ'] = 'Ae';
+        $chars['æ'] = 'ae';
+        $chars['Ø'] = 'Oe';
+        $chars['ø'] = 'oe';
+        $chars['Å'] = 'Aa';
+        $chars['å'] = 'aa';
+        $chars['l·l'] = 'll';
+        $chars['Đ'] = 'DJ';
+        $chars['đ'] = 'dj';
+        return strtr( $string, $chars );
     }
 }
